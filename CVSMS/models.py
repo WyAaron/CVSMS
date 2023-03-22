4e97f27cea29df3a97753b35c829e8aaca470d9d
WyAaron <aaron_wy@dlsu.edu.ph>
2023-03-22 16:27:41 +0800

    updated models.py

--- CVSMS/models.py
+++ CVSMS/models.py
@@ -2,21 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class storageNode(models.Model): 
-    pass
-
-class userInfo(models.Model): 
-    pass 
-
-class heartBeat(models.Model): 
-    pass
-
 def file_path(instance,filename): 
     return f'{instance.FID}/{filename}'
-
-
-
 class Files(models.Model):
     
     RAID_TYPE_CHOICES = [
@@ -31,11 +18,36 @@ class Files(models.Model):
     owner = models.CharField(User, null=True,blank=True,max_length=300)
     FID = models.IntegerField(null=True)
     SID = models.IntegerField(null=True)
-    fileName = models.CharField(null=True, max_length=300)
+    fileName = models.CharField(null=True, max_length=256)
     file = models.FileField(null=True,upload_to=file_path)    
     actualSize = models.IntegerField(null=True)
     start = models.IntegerField(null=True)
     RAIDtype = models.CharField(null=True,choices=RAID_TYPE_CHOICES,default="NONE",max_length=7)
     RAIDid = models.IntegerField(null=True,default=-1)
     
+
+class storageNodeStatus(models.Model): 
+    IP = models.GenericIPAddressField()
+    port = models.IntegerField()
+    status = models.BooleanField(default=False)
+    time = models.TimeField()
+
+class storageNodeInfo(models.Model): 
+    SID = models.IntegerField()
+    allocSize = models.IntegerField() 
+    IP = models.GenericIPAddressField()
+    port = models.IntegerField()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
