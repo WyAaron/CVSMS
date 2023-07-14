2253d5a69cd9fa876a25a13e6c64eac076e2e0d6
WyAaron <80665144+WyAaron@users.noreply.github.com>
2023-07-15 00:27:07 +0800

    updated the filename

--- CVSMS/models.py
+++ CVSMS/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import get_valid_filename
 
 # Create your models here.
 
 
 def file_path(instance, filename):
-    return f'{instance.FID}/{filename}'
+    return f'{instance.FID}/{get_valid_filename(filename)}'
 
 
 class Files(models.Model):
